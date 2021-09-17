f3f9072341d88dc30a2eafa2512d5d6b4af65988
Ian Wolterstorff <wolterstorff.i@northeastern.edu>
2021-09-17 13:07:51 -0400

    Proof of concept

---
+++ happyframe-client.ino
@@ -0,0 +1,44 @@
+/******************************************************************
+ This is an example for the Adafruit RA8875 Driver board for TFT displays
+ ---------------> http://www.adafruit.com/products/1590
+ The RA8875 is a TFT driver for up to 800x480 dotclock'd displays
+ It is tested to work with displays in the Adafruit shop. Other displays
+ may need timing adjustments and are not guanteed to work.
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source hardware
+ by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries.
+ BSD license, check license.txt for more information.
+ All text above must be included in any redistribution.
+ ******************************************************************/
+
+#include <SPI.h>
+#include "Adafruit_RA8875.h"
+
+
+// Library only supports hardware SPI at this time
+// Connect SCLK to UNO Digital #13 (Hardware SPI clock)
+// Connect MISO to UNO Digital #12 (Hardware SPI MISO)
+// Connect MOSI to UNO Digital #11 (Hardware SPI MOSI)
+#define RA8875_INT 4
+#define RA8875_CS 16
+#define RA8875_RESET 5
+
+Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
+
+void setup() {
+    while (!tft.begin(RA8875_800x480));
+    tft.displayOn(true);
+    tft.GPIOX(true);
+    tft.PWM1config(true, RA8875_PWM_CLK_DIV1024);
+    tft.PWM1out(255);
+}
+
+void loop() {
+    tft.fillScreen(RA8875_WHITE);
+    delay(500);
+    tft.fillScreen(RA8875_RED);
+    delay(500);
+}
