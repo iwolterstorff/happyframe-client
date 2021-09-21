938f2eefd1c7887f9ffcc90bafb93e340a0da70c
Ian Wolterstorff <wolterstorff.i@northeastern.edu>
2021-09-20 23:43:28 -0400

    Writing infrastructure code for client

--- happyframe-client.ino
+++ happyframe-client.ino
@@ -1,21 +1,11 @@
-/******************************************************************
- This is an example for the Adafruit RA8875 Driver board for TFT displays
- ---------------> http://www.adafruit.com/products/1590
- The RA8875 is a TFT driver for up to 800x480 dotclock'd displays
- It is tested to work with displays in the Adafruit shop. Other displays
- may need timing adjustments and are not guanteed to work.
-
- Adafruit invests time and resources providing this open
- source code, please support Adafruit and open-source hardware
- by purchasing products from Adafruit!
-
- Written by Limor Fried/Ladyada for Adafruit Industries.
- BSD license, check license.txt for more information.
- All text above must be included in any redistribution.
- ******************************************************************/
-
 #include <SPI.h>
-#include "Adafruit_RA8875.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <LittleFS.h>
+#include <Adafruit_RA8875.h>
+
+// define a credentials.h defining SSID and optionally WPA_KEY
+#include "credentials.h"
 
 
 // Library only supports hardware SPI at this time
@@ -26,14 +16,29 @@
 #define RA8875_CS 16
 #define RA8875_RESET 5
 
+// The number of pixels to buffer for loading
+#define BUFFPIXEL 20
+
 Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
 
 void setup() {
+    #ifdef WPA_KEY
+        WiFi.begin(SSID, WPA_KEY);
+    #else
+        WiFi.begin(SSID);
+    #endif
+    while (WiFi.status() != WL_CONNECTED);
+
     while (!tft.begin(RA8875_800x480));
     tft.displayOn(true);
     tft.GPIOX(true);
     tft.PWM1config(true, RA8875_PWM_CLK_DIV1024);
     tft.PWM1out(255);
+
+    LittleFS.begin();
+    if (LittleFS.exists("/image.bmp")) {
+        display_bmp(LittleFS.open("/image.bmp", "r"));
+    }
 }
 
 void loop() {
@@ -41,4 +46,153 @@ void loop() {
     delay(500);
     tft.fillScreen(RA8875_RED);
     delay(500);
+}
+
+void request_and_save_file(String url) {
+    HTTPClient http;
+    WiFiClient wifi;
+    http.begin(wifi, url);
+    int httpCode = http.GET();
+    if (httpCode == HTTP_CODE_OK && http.header("Content-Type").indexOf("image/bmp") >= 0) {
+        File image = LittleFS.open("/image.bmp", "w");
+        int len = http.getSize();
+        uint8_t buff[128] = { 0 };
+        WiFiClient* stream = http.getStreamPtr();
+        while (http.connected() && (len > 0 || len == -1)) {
+            size_t size = stream->available();
+            if (size) {
+                // Read up to 128 bytes
+                int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+                image.write(buff, c);
+                if (len > 0) {
+                    len -= c;
+                }
+            }
+            delay(1);
+        }
+    }
+    http.end();
+}
+
+// Adapted from https://github.com/adafruit/Adafruit_RA8875/blob/master/examples/ra8875_bitmap_fast/ra8875_bitmap_fast.ino
+#define BUFFPIXEL 20
+
+void display_bmp(File image) {
+    int bmpWidth, bmpHeight;
+    uint8_t bmpDepth; // Bit depth (must be 24 currently)
+    uint32_t bmpImageOffset;
+    uint32_t rowSize;
+    uint8_t fsbuffer[3 * BUFFPIXEL]; // (R+G+B per pixel)
+    uint16_t lcdbuffer[BUFFPIXEL]; // (16-bit per pixel for display)
+    uint8_t buffidx = sizeof(fsbuffer);
+    boolean goodBmp = false;
+    boolean flip = true;
+    int w, h, row, col, xpos, ypos;
+    uint8_t r, g, b;
+    uint32_t pos = 0;
+    uint8_t lcdidx = 0;
+    boolean first = true;
+
+    if (read16(image) == 0x4D42) { // BMP header magic number
+        (void) read32(image); // read and throw out file size
+        (void) read32(image); // read and throw out creator metadata
+        bmpImageOffset = read32(image); // Save index of the start of image data
+        (void) read32(image); // Throw out start of DIB header
+        bmpWidth = read32(image);
+        bmpHeight = read32(image);
+        if (bmpWidth == 800 && bmpHeight == 480) {
+            if (read16(image) == 1) { // Must be only 1 plane
+                bmpDepth = read16(image);
+                if ((bmpDepth == 24) && (read32(image) == 0)) { // 0 = uncompressed
+                    goodBmp = true;
+
+                    // BMP rows are padded (if needed) to 4-byte boundary
+                    rowSize = (bmpWidth * 3 + 3) & ~3;
+
+                    // If bmpHeight is negative, image is in top-down order as opposed to canonical bottom-up
+                    if (bmpHeight < 0) {
+                        bmpHeight = -bmpHeight;
+                        flip = false;
+                    }
+                    w = tft.width();
+                    h = tft.height();
+                    ypos = 0;
+                    xpos = 0;
+                    for (row = 0; row < h; ++row) {
+                        // For each scanline seek to start of scanline
+                        if (flip) {
+                            pos = bmpImageOffset + (bmpHeight - 1 - row) * rowSize;
+                        } else {
+                            pos = bmpImageOffset + row * rowSize;
+                        }
+
+                        if (image.position() != pos) {
+                            image.seek(pos);
+                            buffidx = sizeof(fsbuffer);
+                        }
+
+                        for (col = 0; col < w; ++col) {
+                            if (buffidx >= sizeof(fsbuffer)) {
+                                if (lcdidx > 0) {
+                                    tft.drawPixels(lcdbuffer, lcdidx, xpos, ypos);
+                                    xpos += lcdidx;
+                                    lcdidx = 0;
+                                    first = false;
+                                }
+
+                                image.read(fsbuffer, sizeof(fsbuffer));
+                                buffidx = 0;
+                            }
+
+                            // Convert pixel from BMP to TFT format
+                            b = fsbuffer[buffidx++];
+                            g = fsbuffer[buffidx++];
+                            r = fsbuffer[buffidx++];
+                            lcdbuffer[lcdidx++] = color565(r, g, b);
+                            if (lcdidx >= sizeof(lcdbuffer) || (xpos + lcdidx) >= w) {
+                                tft.drawPixels(lcdbuffer, lcdidx, xpos, ypos);
+                                lcdidx = 0;
+                                xpos += lcdidx;
+                            }
+                        } // end pixel
+                        ++ypos;
+                    } // end scanline
+
+                    // Write any remaining data to LCD
+                    if (lcdidx > 0) {
+                        tft.drawPixels(lcdbuffer, lcdidx, xpos, ypos);
+                        xpos += lcdidx;
+                    }
+                } // End goodBmp
+            }
+        }
+    }
+
+    image.close();
+    //if (!goodBmp) // IDK cry and stuff
+}
+
+
+// These read 16- and 32-bit types from the file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File f) {
+    uint16_t result;
+    ((uint8_t *)&result)[0] = f.read(); // LSB
+    ((uint8_t *)&result)[1] = f.read(); // MSB
+    return result;
+}
+
+uint32_t read32(File f) {
+    uint32_t result;
+    ((uint8_t *)&result)[0] = f.read(); // LSB
+    ((uint8_t *)&result)[1] = f.read();
+    ((uint8_t *)&result)[2] = f.read();
+    ((uint8_t *)&result)[3] = f.read(); // MSB
+    return result;
+}
+
+uint16_t color565(uint8_t r, uint8_t g, uint8_t b) {
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
